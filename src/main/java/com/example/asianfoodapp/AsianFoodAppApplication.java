032085fc147d8f2f9c662cc51dd4e0302369f46d
Tamiol <gnieznoap@gmail.com>
2023-03-01 08:18:04 +0100

    init

---
+++ src/main/java/com/example/asianfoodapp/AsianFoodAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.asianfoodapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsianFoodAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsianFoodAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/asianfoodapp/catalog/domain/Recipe.java
@@ -0,0 +1,5 @@
+package com.example.asianfoodapp.catalog.domain;
+
+public class Recipe {
+
+}
