82aa7529ef97065811766af102c6f202eb69c0eb
Tamiol <gnieznoap@gmail.com>
2023-03-13 20:24:29 +0100

    connection to database, entities

--- src/main/java/com/example/asianfoodapp/catalog/domain/Ingredient.java
+++ src/main/java/com/example/asianfoodapp/catalog/domain/Ingredient.java
@@ -1,20 +1,26 @@
 package com.example.asianfoodapp.catalog.domain;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Ingredient {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private double amount;
     private String unit;
-    private ArrayList<Recipe> recipes;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private Set<Recipe> recipes = new HashSet<>();
 
 }
--- src/main/java/com/example/asianfoodapp/catalog/domain/Recipe.java
+++ src/main/java/com/example/asianfoodapp/catalog/domain/Recipe.java
@@ -4,17 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-
+import java.util.HashSet;
+import java.util.Set;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@Entity
 public class Recipe {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String name;
-    private ArrayList<Ingredient> ingredients;
+
+    private LocalDateTime createdAt;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private Set<Ingredient> ingredients = new HashSet<>();
     private int readyInMinutes;
     private String instructions;
     private boolean vegetarian;
